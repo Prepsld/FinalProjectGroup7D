d8b421e2c122737891bee7547ed5c4aa1bff03a6
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-04-26 01:50:10 -0600

    Update MainPage.xaml.cs
    Make Main page the same style as Utilities page, using the database methods so as to re-use code.

--- FinalProjectGroup7D/MainPage.xaml.cs
+++ FinalProjectGroup7D/MainPage.xaml.cs
@@ -1,44 +1,24 @@
-﻿using Microsoft.Maui.Controls;
+﻿using FinalProjectGroup7D.Database;
+using FinalProjectGroup7D.Tables;
+using Microsoft.Maui.Controls;
 using MySqlConnector;
 
 namespace FinalProjectGroup7D;
 
 public partial class MainPage : ContentPage
+
+
 {
-	public MainPage()
+    public List<User> Users = new List<User>();
+
+    public MainPage()
     {
         InitializeComponent();
 
-        string connectionString = "Server=localhost;Uid=root;Pwd=password;Database=oopfinal;";
+        ProjectDatabase dataBase = new ProjectDatabase();
 
-        // connection is created and is properly garbage collected when the connection is closed
-        using (MySqlConnection connection = new MySqlConnection(connectionString))
-        {
-            connection.Open();
+        Users = dataBase.SelectUser();
 
-            // hardcoded MariaDB compatible SQL query
-            string query = "SELECT * FROM user LIMIT 1;";
-            using (MySqlCommand command = new MySqlCommand(query, connection))
-            {
-                // retrieves data from the database by executing the query with the proper connection details
-                using (MySqlDataReader reader = command.ExecuteReader())
-                {
-                    // if there is data to be read with the results from command.ExecuteReader()
-                    // parses and sets corresponding variables from the rows of data retrieved from command.ExecuteReader()
-                    if (reader.Read())
-                    {
-                        string firstName = reader.GetString("first_name");
-                        string lastName = reader.GetString("last_name");
-                        string email = reader.GetString("e_mail");
-
-                        firstNameLabel.Text = firstName;
-                        lastNameLabel.Text = lastName;
-                        emailLabel.Text = email;
-                    }
-                }
-            }
-            connection.Close();
-        }
+        UserList.ItemsSource = Users;
     }
 }
-
